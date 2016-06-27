29a91077898b2a03180a28a3c8b9b5791b7ac339
nijin39 <nijin39@gmail.com>
2016-06-27 17:31:42 +0000

    2016. 06. 28 Repository Source

--- src/main/java/com/tandem6/springedu/EducationApplication.java
+++ src/main/java/com/tandem6/springedu/EducationApplication.java
@@ -1,13 +1,69 @@
 package com.tandem6.springedu;
 
+
+
+
+import org.h2.server.web.WebServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.tandem6.springedu.domain.Customer;
+import com.tandem6.springedu.repository.CustomerRepository;
 
 @SpringBootApplication
 public class EducationApplication {
+    
+    private static final Logger log = LoggerFactory.getLogger(EducationApplication.class);
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(EducationApplication.class, args);
 	}
+	
+	@Bean
+public ServletRegistrationBean h2servletRegistration() {
+    ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+    registration.addUrlMappings("/console/*");
+    return registration;
+}
 
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			// save a couple of customers
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+            log.info("");
+
+			// fetch an individual customer by ID
+			Customer customer = repository.findOne(1L);
+			log.info("Customer found with findOne(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+            log.info("");
+
+			// fetch customers by last name
+			log.info("Customer found with findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			for (Customer bauer : repository.findByLastName("Bauer")) {
+				log.info(bauer.toString());
+			}
+            log.info("");
+		};
+	}
 }
