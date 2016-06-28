2e438bc60e13272f609b8d62e978e761337c2fb6
nijin39 <nijin39@gmail.com>
2016-06-28 00:10:10 +0000

    2016. 06. 28 Customer Example

--- src/main/java/com/tandem6/springedu/controller/EducationController.java
+++ src/main/java/com/tandem6/springedu/controller/EducationController.java
@@ -1,14 +1,28 @@
 package com.tandem6.springedu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tandem6.springedu.domain.Customer;
+import com.tandem6.springedu.service.EducationService;
 
 
 @RestController
 public class EducationController {
     
+    @Autowired
+    EducationService educationService;
+    
+    // /로 들어온 요청에 대한 처리
+    // Say HI를 Http Response에 실어서 내보낸다. 
     @RequestMapping("/")
     public String sayHello(){
         return "say HI";
     } 
+    
+    @RequestMapping("/customers")
+    public Iterable<Customer> getCustomerList(){
+        return educationService.listAllCustomer();
+    }
 }
