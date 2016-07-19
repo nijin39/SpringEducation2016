26032bedddde487751c4fe0f19f2d21495bd6a57
KIM JONG IL <nijin39@gmail.com>
2016-07-19 00:11:34 +0000

    JSP 201707

--- src/main/java/com/tandem6/springedu/controller/WelcomeController.java
+++ src/main/java/com/tandem6/springedu/controller/WelcomeController.java
@@ -2,7 +2,10 @@ package com.tandem6.springedu.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
 import java.util.Map;
@@ -17,8 +20,25 @@ public class WelcomeController {
     public String welcome(Map<String, Object> model) {
         model.put("time", new Date());
         model.put("message", this.message);
-        System.out.println("ZZZZ");
+        System.out.println("ZZZ");
         return "welcome";
+    }
+    
+    @RequestMapping("/sayhello/{name}")
+    public ModelAndView sayhello(@PathVariable String name){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("say");
+        mav.addObject("name", name);
+        return mav;
+    }
+    
+    // /query?name=lee
+    @RequestMapping("/query")
+    public ModelAndView query(@RequestParam(value="name",required=true) String name){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("query");
+        mav.addObject("name",name);
+        return mav;
     }
 
     @RequestMapping("/foo")
